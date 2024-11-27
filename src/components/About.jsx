017c9c37d27345f5bd28978ad1ec929f6b5480fc
sivaraman33 <146847596+sivaraman33@users.noreply.github.com>
2024-11-27 17:20:03 +0530

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import AboutImage from "../assets/about.png";
+
+const About = () => {
+  return (
+    <div className="bg-white text-black py-20" id="about">
+      <div className="container mx-auto px-8 md:px-16 lg:px-24">
+        <h2 className="text-4xl font-bold text-center mb-12">About Me</h2>
+        <div className="flex flex-col md:flex-row items-center md:space-x-12">
+          <img
+            src={AboutImage}
+            alt=""
+            className="w-72 h-80 rounded object-cover mb-8 md:mb-0"
+          />
+          <div className="flex-1">
+            <p className="text-lg mb-8">
+              I am a dedicated Web Devoleper with a focus on building
+              modern and responsive web applications. With a strong foundation
+              in both frontend and backend technologies, I strive to create
+              seamless and efficient user experiences.
+              
+              I’m always eager to learn new skills and stay updated
+               with the latest web development trends to deliver high-quality, efficient solutions.
+            </p>
+            <div className="space-y-4">
+              <div className="flex items-center">
+                <label htmlFor="html and css" className="w-2/12">
+                  React JS
+                </label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div
+                    className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full 
+                    transform transition-transform duration-300 hover:scale-105 w-10/12"
+                  ></div>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <label htmlFor="html and css" className="w-2/12">
+                  Node Js
+                </label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div
+                    className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full 
+                    transform transition-transform duration-300 hover:scale-105 w-11/12"
+                  ></div>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <label htmlFor="html and css" className="w-2/12">
+                  HTML&CSS
+                </label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div
+                    className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full 
+                    transform transition-transform duration-300 hover:scale-105 w-9/12"
+                  ></div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-12 flex justify-between text-center">
+              <div>
+                <h3 className="text-2xl font-bold text-transparent bg-clip-text 
+                bg-gradient-to-r from-green-400 to-blue-500">
+                  3+
+                </h3>
+                <p>Years of Experience</p>
+              </div>
+              <div>
+                <h3 className="text-2xl font-bold text-transparent bg-clip-text 
+                bg-gradient-to-r from-green-400 to-blue-500">
+                  53+
+                </h3>
+                <p>Projects are Completed</p>
+              </div>
+              <div>
+                <h3 className="text-2xl font-bold text-transparent bg-clip-text 
+                bg-gradient-to-r from-green-400 to-blue-500">
+                  12+
+                </h3>
+                <p>Happy Clients</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
